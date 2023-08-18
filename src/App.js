a4277f0d470dd8273a2773e53b24eb971358cc24
Muhammad Awais <137446210+muhammad7865@users.noreply.github.com>
2023-08-18 12:53:08 +0500

    Added backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 
+import React, { useState } from 'react'
+
+import Navbar from './Components/Navbar';
+import Alert from './Components/Alert'
+import Home from './Components/Home';
+import About from './Components/About';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+
 function App() {
+
+  const toggelmode = () => {
+    if (mood === "dark") {
+      setmood("light")
+      document.body.style.backgroundColor = "white"
+      setAlert("Light mood has been enabled", "success")
+    }
+    else {
+      setmood("dark")
+      document.body.style.backgroundColor = "#051e41"
+      setAlert("Dark mood has been enabled", "primary")
+    }
+  }
+
+  const [mood, setmood] = useState("light")
+  const [alert, setAlert] = useState(null)
+
+
   return (
-    <div className="App">
-      This is the my third project on react app
-    </div>
-  );
+    <>
+    <Router>
+      <Navbar title="iNoteBook" second="About" status={mood} toggel={toggelmode} />
+      <Alert alert={alert} />
+
+      <Routes>
+      <Route exact path="/" element={<Home></Home>}></Route>
+      <Route exact path="/about" element={<About></About>}></Route>
+      </Routes>
+     
+    </Router>
+    </>
+  )
 }
 
 export default App;
